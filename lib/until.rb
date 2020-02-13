401c3cec1542682ea999a82e33d496036d0c3b8c
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-13 03:40:41 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,34 @@
-def using_until
-  levitation_force = 6
-  until levitation_force == 10
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def using_until
+#   levitation_force = 6
+#   until levitation_force == 10
+#     puts "Wingardium Leviosa"
+#     levitation_force += 1
+#   end
+# end
